0ff9b38323792c686f7dae4ee79b2e908953e9b6
AlekseyKoks-in <alekseykoksin@gmail.com>
2021-09-11 00:38:08 +0300

    educational project "box of sweets"

--- src/com/alex/study/Main.java
+++ src/com/alex/study/Main.java
@@ -3,16 +3,11 @@ package com.alex.study;
 public class Main {
 
     public static void main(String[] args) {
-        Candy c1 = new Candy("candy1", 10.5, 100);
-        Candy c2 = new Candy("candy2", 11.5, 110);
-        Chocolate ch1 = new Chocolate("chocolate1", 15.5, 150);
-        Chocolate ch2 = new Chocolate("chocolate2", 16.5, 160);
-
         BoxWithSweets b1 = new BoxWithSweets();
-        b1.addTheSweet(c1);
-        b1.addTheSweet(c2);
-        b1.addTheSweet(ch1);
-        b1.addTheSweet(ch2);
+        b1.addTheSweet(new Candy("candy1", 10.5, 100));
+        b1.addTheSweet(new Candy("candy2", 11.5, 110));
+        b1.addTheSweet(new Chocolate("chocolate1", 15.5, 150));
+        b1.addTheSweet(new Chocolate("chocolate2", 16.5, 160));
 
         b1.printPrice();
         b1.printWeight();
